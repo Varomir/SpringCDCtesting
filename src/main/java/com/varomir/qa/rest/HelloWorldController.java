8d2d39747a0cf197118dec0d71f81742c7bd6548
Vadym Feshchenko <VFeshchenko@ten-x.com>
2019-12-05 23:25:25 +0200

    Expose 'findByLastName' lookup via HTTP endpoint

--- src/main/java/com/varomir/qa/rest/HelloWorldController.java
+++ src/main/java/com/varomir/qa/rest/HelloWorldController.java
@@ -1,13 +1,35 @@
 package com.varomir.qa.rest;
 
+import com.varomir.qa.domain.Person;
+import com.varomir.qa.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class HelloWorldController {
 
+    private final PersonRepository personRepository;
+
+    @Autowired
+    public HelloWorldController(final PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @GetMapping("/hello/{lastName}")
+    public String hello(@PathVariable final String lastName) {
+        Optional<Person> foundPerson = personRepository.findByLastName(lastName);
+
+        return foundPerson
+                .map(person -> String.format("Hello %s %s!", person.getFirstName(), person.getLastName()))
+                .orElse(String.format("Who is this '%s' you're talking about?", lastName));
     }
 }
