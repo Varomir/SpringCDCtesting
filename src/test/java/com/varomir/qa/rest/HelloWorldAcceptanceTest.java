1fbca8eaef31be9a84c89b7bdc4a2c44009917cc
Vadym Feshchenko <VFeshchenko@ten-x.com>
2019-12-05 13:57:45 +0200

    Add 'Acceptance test' HTTP example based on RestAssured library

---
+++ src/test/java/com/varomir/qa/rest/HelloWorldAcceptanceTest.java
@@ -0,0 +1,36 @@
+package com.varomir.qa.rest;
+
+import com.varomir.qa.DemoApp;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.containsString;
+
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {DemoApp.class})
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@Execution(ExecutionMode.CONCURRENT)
+public class HelloWorldAcceptanceTest {
+
+    @DisplayName("'/hello' endpoint should return expected response body text via HTTP protocol")
+    @Test
+    void shouldReturnExpectedGreetingsViaHTTP() {
+        System.out.println(">> From 'HelloWorldAcceptanceTest.shouldReturnExpectedGreetingsViaHTTP()' Thread_ID: "
+                + Thread.currentThread().getId());
+        when()
+                .get("http://localhost:8080/hello")
+        .then()
+                .statusCode(is(200))
+                .body(containsString("Hello World!"));
+    }
+}
