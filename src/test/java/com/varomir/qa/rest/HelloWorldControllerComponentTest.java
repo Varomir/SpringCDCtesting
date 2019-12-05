4c4945aab49ec980eb085de1d35a9f5e6ad2ce8d
Vadym Feshchenko <VFeshchenko@ten-x.com>
2019-12-05 23:30:45 +0200

    Add 2 new 'PersonRepository' component test wit @MockBean, disable concurent test execution here

--- src/test/java/com/varomir/qa/rest/HelloWorldControllerComponentTest.java
+++ src/test/java/com/varomir/qa/rest/HelloWorldControllerComponentTest.java
@@ -1,31 +1,62 @@
 package com.varomir.qa.rest;
 
-import com.varomir.qa.DemoApp;
+import com.varomir.qa.domain.Person;
+import com.varomir.qa.repository.PersonRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
+
 @ExtendWith(SpringExtension.class)
 @WebMvcTest
-@Execution(ExecutionMode.CONCURRENT)
+// https://docs.spring.io/spring-framework/docs/current/spring-framework-reference/testing.html#testcontext-parallel-test-execution
+@Execution(ExecutionMode.SAME_THREAD)
 public class HelloWorldControllerComponentTest {
 
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
+    @MockBean
+    private PersonRepository personRepository;
+
+
+    @DisplayName("'/hello' endpoint should return expected greetings when GET")
     @Test
     void shouldReturnHelloWorld() throws Exception {
-        System.out.println(">> From 'HelloWorldControllerComponentTest.shouldReturnHelloWorld()' Thread_ID: " + Thread.currentThread().getId());
-
         mockMvc.perform(get("/hello"))
                 .andExpect(content().string("Hello World!"));
+    }
+
+    @DisplayName("'/hello/{lastName}' endpoint should return expected greetings for the existing Person when GET")
+    @Test
+    void shouldReturnPersonFullName() throws Exception {
+        Person johnDoe = new Person("John", "Doe");
+        given(personRepository.findByLastName("Doe")).willReturn(Optional.of(johnDoe));
+
+        mockMvc.perform(get("/hello/Doe"))
+                .andExpect(content().string("Hello John Doe!"));
+    }
+
+    @DisplayName("'/hello/{lastName}' endpoint should return expected error greetings for the unknown Person when GET")
+    @Test
+    void shouldReturnMessageIfPersonIsUnknown() throws Exception {
+        given(personRepository.findByLastName(anyString())).willReturn(Optional.empty());
+
+        mockMvc.perform(get("/hello/Doe"))
+                .andExpect(content().string("Who is this 'Doe' you're talking about?"));
     }
 }
